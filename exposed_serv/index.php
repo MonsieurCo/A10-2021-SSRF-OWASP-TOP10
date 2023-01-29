386ad656fc7809a320985d786af4a30e593bc623
Dylann Batisse <dylannbatisse@gmail.com>
2023-01-29 17:36:14 +0100

    protect

--- exposed_serv/index.php
+++ exposed_serv/index.php
@@ -9,9 +9,9 @@ if (isset($_GET['file'])) {
 }
 
 // ssrf request ifconfig.pro
-if (isset($_GET['url'])) {
+if (isset($_GET['curl'])) {
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $_GET['url']);
+    curl_setopt($ch, CURLOPT_URL, $_GET['curl']);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $output = curl_exec($ch);
     curl_close($ch);
@@ -26,32 +26,35 @@ if (isset($_GET['fopen'])) {
     fclose($file);
 }
 
-// ssrf request with post
-if (isset($_POST['animals'])) {
-    echo $_POST['animals'];
-    $file = fopen($_POST['animals'], "r");
-    echo fread($file, filesize($_GET['fopen']));
-    fclose($file);
-}
-
 ?>
 
 <html>
 
 <head>
-    <title>SSRF</title>
+  <title>SSRF</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 
 <body>
-    <h1>SSRF</h1>
-    <p>Simple SSRF script</p>
-    <p>Usage: http://localhost:8080/ssrf.php?url=http://ifconfig.pro </a>
-    <p>Usage: http://localhost:8080/ssrf.php?file=/etc/passwd</p>
-    <p>Usage: http://localhost:8080/ssrf.php?fopen=/etc/passwd</p>
-    <!-- SSRF form action post -->
-    <form action="ssrf.php" id="carform" method="post">
-        <input type="submit" id="submit_form">
-    </form>
+  <div class="container">
+    <h1 class="text-center">SSRF</h1>
+    <div class="card text-center mb-3">
+      <div class="card-body">
+        <p><b>file_get_contents</b> | http://localhost:8080/?file=http://ifconfig.pro </p>
+      </div>
+    </div>
+    <div class="card text-center mb-3">
+      <div class="card-body">
+        <p><b>fopen</b> | http://localhost:8080/?fopen=/etc/passwd </p>
+      </div>
+    </div>
+    <div class="card text-center mb-3">
+      <div class="card-body">
+        <p><b>curl</b> | http://localhost:8080/?curl=http://ifconfig.pro</p>
+      </div>
+    </div>
+  </div>
 </body>
 
 </html>
+
--- exposed_serv/ssrf_protected.php
+++ exposed_serv/ssrf_protected.php
@@ -1,7 +1,7 @@
 <?php
-header("Access-Control-Allow-Origin: 'self'");
 
-function disable_url_input($str) {
+function is_valid_url($str)
+{
     return filter_var($str, FILTER_VALIDATE_URL);
 }
 
@@ -11,22 +11,26 @@ function disable_url_input($str) {
 // vulnerability in the "SSRF" challenge.
 // show file content
 if (isset($_GET['file'])) {
-    if (disable_url_input($_GET['file'])) {
-        echo "URL not allowed";
+    $file = $_GET['file'];
+    if (strpos($file, 'ressource') !== false and strpos($file, '/..') == false and strpos($file, '/.') == false) {
+        if (!is_valid_url($file) && (strpos($file, '.jpg') !== false or strpos($file, '.png') !== false)) {
+            echo '<img src="', $file, '" alt="', '" />';
+        } else {
+            echo file_get_contents($file);
+        }
     } else {
-        echo file_get_contents($_GET['file']);
+        echo "Not allowed";
     }
 }
 
 // ssrf request ifconfig.pro
-if (isset($_GET['url'])) {
-    $url = $_GET['url'];
-    // make sure the url is only from localhost 
+if (isset($_GET['curl'])) {
+    $url = $_GET['curl'];
+    // make sure the url is only from localhost
     if (strpos($url, 'localhost') !== false) {
         $url = "http://" . $url;
     } else {
         echo "Not allowed";
-        exit();
     }
     // ssrf request with curl
     $ch = curl_init();
@@ -54,17 +58,9 @@ if (isset($_GET['fopen'])) {
         fclose($file);
     } else {
         echo "Not allowed";
-        exit();
     }
 }
 
-// ssrf request with post
-if (isset($_POST['animals'])) {
-    echo $_POST['animals'];
-    $file = fopen($_POST['animals'], "r");
-    echo fread($file, filesize($_GET['fopen']));
-    fclose($file);
-}
 
 ?>
 
@@ -72,18 +68,28 @@ if (isset($_POST['animals'])) {
 
 <head>
     <title>SSRF</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 
 <body>
-    <h1>SSRF</h1>
-    <p>Simple SSRF script</p>
-    <p>Usage: http://localhost:8080/ssrf.php?url=http://ifconfig.pro </a>
-    <p>Usage: http://localhost:8080/ssrf.php?file=/etc/passwd</p>
-    <p>Usage: http://localhost:8080/ssrf.php?fopen=/etc/passwd</p>
-    <!-- SSRF form action post -->
-    <form action="ssrf.php" id="carform" method="post">
-        <input type="submit" id="submit_form">
-    </form>
+    <div class="container">
+        <h1 class="text-center">SSRF</h1>
+        <div class="card text-center mb-3">
+            <div class="card-body">
+                <p><b>file_get_contents</b> | http://localhost:8080/ssrf_protected.php?file=http://ifconfig.pro </p>
+            </div>
+        </div>
+        <div class="card text-center mb-3">
+            <div class="card-body">
+                <p><b>fopen</b> | http://localhost:8080/ssrf_protected.php?fopen=/etc/passwd </p>
+            </div>
+        </div>
+        <div class="card text-center mb-3">
+            <div class="card-body">
+                <p><b>curl</b> | http://localhost:8080/ssrf_protected.php?curl=http://ifconfig.pro</p>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
